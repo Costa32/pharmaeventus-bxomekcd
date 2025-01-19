b07cb367667a99666043f67aaba4f3a44da9c7c7
Costa32 <84142991+Costa32@users.noreply.github.com>
2025-01-19 11:47:33 -0300

    Update contactInfo.ts

--- src/data/contactInfo.ts
+++ src/data/contactInfo.ts
@@ -2,7 +2,7 @@ export const contactInfo = {
   email: "contato@pharmaeventus.com.br",
   phone: "(71) 3561-0260",
   address: {
-    building: "Rua Alceu Amoroso de Lima, 786",
+    street: "Rua Alceu Amoroso de Lima, 786",
     building: "Edf. Tancredo Neves Trade",
     suite: "Sala 312",
     neighborhood: "Caminho das Árvores",
